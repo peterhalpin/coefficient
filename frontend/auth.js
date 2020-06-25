eb5a58ef5aece22dc01a209e0f36f03900fdb73a
Christina Alperi <christinaalperi@gmail.com>
2020-06-25 17:29:44 -0400

    updated framework for uploading files

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,15 +1,11 @@
-// Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 var uiConfig = {
-    signInSuccessUrl: 'main.html',
-    signInFlow: 'popup',
-    signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    // Other config options...
+  signInSuccessURL: 'upload.html',
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  // Other config options...
   };
 
-  ui.start('#firebaseui-auth-container', uiConfig)
-
-  
+ui.start('#firebaseui-auth-container', uiConfig);
