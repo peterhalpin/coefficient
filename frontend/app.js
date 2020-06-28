2d85f690a2d8d90e34a5f701f80b7595a166d8c5
Christina Alperi <christinaalperi@gmail.com>
2020-06-28 19:17:30 -0400

    file uploads to firebase storage

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,22 @@
-const api = require('firebase-tools/lib/api');
+var htmlFileButton = document.getElementById("html");
+htmlFileButton.addEventListener('change', function(e) {
+    var html = e.target.files[0]; 
+    var storageRef = firebase.storage().ref(html.name);
+    var task = storageRef.put(html);
+
+});
+  
+
+var cssFileButton = document.getElementById("css");
+cssFileButton.addEventListener('change', function(e) {
+    var css = e.target.files[0]; 
+    var storageRef = firebase.storage().ref(css.name);
+    var task = storageRef.put(css);
+});
+  
+var jsFileButton = document.getElementById("js");
+jsFileButton.addEventListener('change', function(e) {
+    var js = e.target.files[0]; 
+    var storageRef = firebase.storage().ref(js.name);
+    var task = storageRef.put(js);
+});
